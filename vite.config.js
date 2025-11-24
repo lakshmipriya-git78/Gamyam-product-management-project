f8e78cb9ee401167cebafad6563f0604a3d1172c
Priya <nanneboyinalakshmipriya39@gmail.com>
2025-11-25 00:37:53 +0530

    Fix index.html for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',   // correct for Vercel
-})
+  base: "./",   // <- THIS FIXES WHITE SCREEN ON VERCEL
+});
+
